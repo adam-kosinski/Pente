3f6e538a4082d0542269e183ac19cdfc75adebfc
Adam <adamkosinski631@gmail.cm>
2024-07-23 02:33:12 -0400

    Initial commit, setting up vue project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="analyze">Analyze</RouterLink>
+    <RouterLink to="play">Play</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
---
+++ src/views/PlayView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>Play</div>
+</template>
