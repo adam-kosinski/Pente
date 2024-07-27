04aeefc0b170a2b75d84f0aabe2e20edd085ddd5
Adam <adamkosinski631@gmail.cm>
2024-07-27 04:11:46 -0400

    Transposition table

--- src/views/AnalyzeView.vue
+++ src/views/AnalyzeView.vue
@@ -3,7 +3,7 @@
 import { ref } from 'vue';
 import Board from '@/components/Board.vue';
 import { createNewGame, makeMove } from '@/model';
-import { findBestMove, evaluatePosition } from '@/engines/engine_v2';
+import { findBestMove, evaluatePosition } from '@/engines/engine_v3';
 
 const game = ref(createNewGame(19))
 
@@ -12,6 +12,8 @@ game.value = JSON.parse('{"board":[{},{},{},{},{},{},{},{"11":0},{"10":1},{"8":1
 
 // game.value = JSON.parse(`
 // {"board":[{},{},{},{},{},{},{},{"11":0},{"10":1,"12":0,"13":1},{"8":1,"9":0,"10":0,"11":0,"12":1,"13":0},{"9":1,"11":1,"14":0},{"10":1,"15":0},{},{},{},{},{},{},{}],"currentPlayer":1,"captures":{"0":0,"1":0},"nMoves":15,"isOver":true,"linearShapes":[{"type":"stretch-two","pattern":"_1_1_","owner":1,"begin":[10,8],"end":[10,12],"length":5,"hash":"stretch-two,1,10,8,10,12"},{"type":"open-pair","pattern":"_00_","owner":0,"begin":[10,10],"end":[7,13],"length":4,"hash":"open-pair,0,10,10,7,13"},{"type":"open-tria","pattern":"_111_","owner":1,"begin":[8,7],"end":[12,11],"length":5,"hash":"open-tria,1,8,7,12,11"},{"type":"open-tessera","pattern":"_1111_","owner":1,"begin":[12,9],"end":[7,14],"length":6,"hash":"open-tessera,1,12,9,7,14"},{"type":"pente-threat-4","pattern":"1111_","owner":1,"begin":[11,10],"end":[7,14],"length":5,"hash":"pente-threat-4,1,11,10,7,14"},{"type":"pente-threat-4","pattern":"_0000","owner":0,"begin":[6,10],"end":[10,14],"length":5,"hash":"pente-threat-4,0,6,10,10,14"},{"type":"pente","pattern":"00000","owner":0,"begin":[7,11],"end":[11,15],"length":5,"hash":"pente,0,7,11,11,15"},{"type":"pente-threat-4","pattern":"0000_","owner":0,"begin":[8,12],"end":[12,16],"length":5,"hash":"pente-threat-4,0,8,12,12,16"}]}`)
+
+game.value = JSON.parse(`{"board":[{},{},{},{},{},{},{},{"11":0,"13":0},{"10":1,"12":0,"13":0},{"8":1,"9":0,"10":0,"11":0,"12":1,"13":0},{"9":1,"10":1,"11":1,"14":1},{},{},{},{},{},{},{},{}],"currentPlayer":1,"captures":{"0":0,"1":0},"nMoves":15,"isOver":false,"linearShapes":[{"type":"open-pair","pattern":"_11_","owner":1,"begin":[8,7],"end":[11,10],"length":4,"hash":"open-pair,1,8,7,11,10"},{"type":"stretch-two","pattern":"_0_0_","owner":0,"begin":[7,10],"end":[7,14],"length":5,"hash":"stretch-two,0,7,10,7,14"},{"type":"open-tria","pattern":"_111_","owner":1,"begin":[10,8],"end":[10,12],"length":5,"hash":"open-tria,1,10,8,10,12"},{"type":"open-pair","pattern":"_00_","owner":0,"begin":[8,11],"end":[8,14],"length":4,"hash":"open-pair,0,8,11,8,14"},{"type":"open-tria","pattern":"_000_","owner":0,"begin":[6,13],"end":[10,13],"length":5,"hash":"open-tria,0,6,13,10,13"},{"type":"capture-threat","pattern":"_110","owner":0,"begin":[11,10],"end":[8,13],"length":4,"hash":"capture-threat,0,11,10,8,13"}]}`)
 
 
 function profile(){
