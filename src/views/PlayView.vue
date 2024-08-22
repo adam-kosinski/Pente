38abbb40f4dc4e8e8ebddebd2f4d771b77537769
Adam <adamkosinski631@gmail.cm>
2024-08-22 18:59:43 -0400

    nicer undo button

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -9,16 +9,16 @@ import { chooseMove } from '@/engine_v19/engine_v19';
 const game = ref(createNewGame(19))
 // game.value = loadFromString("19~9.9|9.7|11.9|11.5|11.7|10.6|8.8|7.7|10.10|12.4|13.3|9.11|12.8|13.9|12.8|11.11|10.9|10.11")
 
-async function playerMove(r: number, c: number){
-  if(game.value.currentPlayer === 1 || game.value.isOver) return
+async function playerMove(r: number, c: number) {
+  if (game.value.currentPlayer === 1 || game.value.isOver) return
   makeMove(game.value, r, c)
-  if(game.value.isOver) return
+  if (game.value.isOver) return
   await new Promise(resolve => setTimeout(resolve, 10))
   const [compR, compC] = chooseMove(game.value, 10, 1000)
   makeMove(game.value, compR, compC)
 }
 
-function goToAnalysis(){
+function goToAnalysis() {
   window.location.href = "/analyze?s=" + gameToString(game.value)
 }
 
@@ -28,11 +28,15 @@ function goToAnalysis(){
 
 
 <template>
-  <button @click="console.log(gameToString(game))">Save Game</button><br>
-  <button @click="undoMove(game)">Undo Move</button><br>
-  <button class="go-to-analysis" @click="goToAnalysis()">
-    <p>Analyze</p><span style="transform: rotateZ(-45deg) translateY(-5%); font-size: 2em;">&#9906;</span>
-  </button><br>
+  <div class="buttons">
+    <button class="go-to-analysis" @click="goToAnalysis()">
+      <p>Analyze</p><span style="transform: rotateZ(-45deg) translateY(-5%); font-size: 2em;">&#9906;</span>
+    </button>
+    <button @click="undoMove(game); undoMove(game)">
+      <p>Undo</p><span style="font-size: 2em;">⎌</span>
+    </button>
+  </div>
+
 
   <div class="board-container">
     <Board class="board" :game="game" :show-coord-labels="false" @make-move="playerMove" />
@@ -41,23 +45,30 @@ function goToAnalysis(){
 
 
 <style scoped>
-
 .board-container {
-  width: min(90vh, calc(100% - 80px));
+  width: min(90vh, calc(100% - 120px));
   height: auto;
   aspect-ratio: 1;
   position: absolute;
   inset: 0;
   margin: auto;
 }
-.go-to-analysis {
+
+.buttons {
   position: absolute;
-  top: 0;
+  top: 10px;
   right: 10px;
+  display: flex;
+  gap: 5px;
+  flex-direction: column;
+  align-items: flex-end;
+}
+.buttons button {
   background-color: transparent;
   border: none;
-  cursor: pointer;
   color: white;
+  cursor: pointer;
+  height: 30px;
 
   display: flex;
   align-items: center;
@@ -66,13 +77,14 @@ function goToAnalysis(){
 
   opacity: 0.5;
 }
-.go-to-analysis p {
+.buttons button p {
   visibility: hidden;
 }
-.go-to-analysis:hover {
+.buttons button:hover {
   opacity: 1;
 }
-.go-to-analysis:hover p {
+.buttons button:hover p {
   visibility: visible;
 }
+
 </style>
