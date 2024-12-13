1932cd3d1096cf35201b809dac2aca680bd0fa0c
Adam <adamkosinski631@gmail.cm>
2024-12-12 23:13:40 -0500

    update test position list

--- src/views/AnalyzeView.vue
+++ src/views/AnalyzeView.vue
@@ -49,17 +49,15 @@ const testPositions = [
   // static evaluation used to be busted in this position, thought white cannot block all of black's pente threats
   // cool position though (and one move ago actually black won!)
   "19~9.9|10.10|6.9|10.6|6.11|12.9|6.10|6.12|6.8|6.7|8.9|7.9|7.10|10.9|8.9|9.8|5.12|4.13|5.10|10.8|10.7|8.10|3.10|4.10|4.11|10.12|10.11|9.11|11.13|9.9|11.9|9.9|7.11|11.7|12.6|7.12|7.11|9.12",
+  // no longer a bug - thinks white won, b/c misses the black move 8.14 because ordered too late, doesn't recognize that a keystone pair is being threatened - need to update move ordering to include keystone capture threats
+  // and perhaps include whether an open pair contains one or two keystones (threatening to capture a pair of keystones should be ranked high)
+  "19~9.9|11.12|6.9|13.10|6.7|13.12|6.8|6.6|7.9|8.9|8.10|5.9|7.11|6.12|7.9|6.9|6.8|9.11|7.10|7.8|5.7|4.6|10.8|11.7|7.12|7.13|5.11",
 
   // CURRENT BUGS
 
   // chooses a dumb move when exiting early from a search at depth 10 - this seems to be because it realized it lost?? but it reports it's winning (-27.9) sometimes
   // I'm struggling to reproduce this issue
   "19~9.9|10.11|13.9|8.9|11.11|10.10|10.12|12.10|10.9|11.9|13.11|9.13|13.8|13.10|14.10|13.7|15.9|12.12|10.8|9.7|14.12|12.10|11.10|12.10|15.13|12.11|16.14|17.15|12.9|12.14|12.13|10.12|8.14|11.11|13.9",
-
-  // thinks white won, b/c misses the black move 8.14 because ordered too late, doesn't recognize that a keystone pair is being threatened - need to update move ordering to include keystone capture threats
-  // and perhaps include whether an open pair contains one or two keystones (threatening to capture a pair of keystones should be ranked high)
-  "19~9.9|11.12|6.9|13.10|6.7|13.12|6.8|6.6|7.9|8.9|8.10|5.9|7.11|6.12|7.9|6.9|6.8|9.11|7.10|7.8|5.7|4.6|10.8|11.7|7.12|7.13|5.11",
-
 ]
 game.value = loadFromString(testPositions[testPositionIndex.value])
 watch(testPositionIndex, i => {
